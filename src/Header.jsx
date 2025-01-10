7b0578e4b9cda5593599ad1ab94c6d924e86720f
plusquam <erwinramos17@gmail.com>
2025-01-10 13:23:55 -0600

    Change img logo gwm

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,13 @@
 import "./styles/header.css";
+import logoGwm from "./assets/gwm-2.png";
+import logoBbva from "./assets/bbva.png";
+
 const Header = () => {
   return (
     <header className="navbar">
       <div className="navbar-content">
-        <img className="logo-bbva" src="../src/assets/bbva.png" alt="" />
-        <img className="logo-gwm" src="../src/assets/gwm-2.png" alt="" />
+        <img className="logo-bbva" src={logoBbva} alt="" />
+        <img className="logo-gwm" src={logoGwm} alt="" />
       </div>
     </header>
   );
