a5bf878fe9a76c33cd6f630d73687b54982786be
plusquam <erwinramos17@gmail.com>
2025-01-10 00:09:53 -0600

    Change svg logo imgs for png images

--- src/FooterNav.jsx
+++ src/FooterNav.jsx
@@ -3,8 +3,8 @@ const FooterNav = () => {
   return (
     <header className="footer-logos">
       <div className="navbar-content-footer">
-        <img className="logo-bbva" src="../src/assets/bbva.svg" alt="" />
-        <img className="logo-gwm" src="../src/assets/gwm.svg" alt="" />
+        <img className="logo-bbva" src="../src/assets/bbva.png" alt="" />
+        <img className="logo-gwm" src="../src/assets/gwm.png" alt="" />
       </div>
     </header>
   );
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,8 +3,8 @@ const Header = () => {
   return (
     <header className="navbar">
       <div className="navbar-content">
-        <img className="logo-bbva" src="../src/assets/bbva.svg" alt="" />
-        <img className="logo-gwm" src="../src/assets/gwm.svg" alt="" />
+        <img className="logo-bbva" src="../src/assets/bbva.png" alt="" />
+        <img className="logo-gwm" src="../src/assets/gwm.png" alt="" />
       </div>
     </header>
   );
