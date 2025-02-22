89d35e98ae34fce36e78d01b64980408f03850c7
plusquam <erwinramos17@gmail.com>
2025-02-21 20:11:37 -0600

    change advice

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -15,14 +15,14 @@ const Footer = () => {
             <div className="container-legal">
               <a
                 target="_blank"
-                href="https://drive.google.com/file/d/1PWeYHJG8RF54NTkJBpACyR2n_lzdoOVB/view?usp=sharing"
+                href="https://www.bbva.mx/personas/aviso-legal.html"
                 className="legal-advices"
               >
                 Aviso legal
               </a>
               <a
                 target="_blank"
-                href="https://www.bbva.mx/personas/aviso-de-privacidad.html"
+                href="https://drive.google.com/file/d/1PWeYHJG8RF54NTkJBpACyR2n_lzdoOVB/view?usp=sharing"
                 className="legal-advices"
               >
                 Avisos de privacidad
